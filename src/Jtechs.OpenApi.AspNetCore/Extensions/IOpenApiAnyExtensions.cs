0e3c2ddc5f47d38650330b31b2101d6af58c0559
Takashi Kawae <77476170+tkawae@users.noreply.github.com>
2024-01-16 22:49:31 +0900

    ソリューションと、テスト以外のプロジェクトを追加

---
+++ src/Jtechs.OpenApi.AspNetCore/Extensions/IOpenApiAnyExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.OpenApi.Any;
+
+namespace Jtechs.OpenApi.AspNetCore.Extensions;
+
+public static class IOpenApiAnyExtensions
+{
+    public static OpenApiArray ToOpenApiArray(this IEnumerable<IOpenApiAny> items)
+    {
+        var array = new OpenApiArray();
+        array.AddRange(items);
+        return array;
+    }
+}
